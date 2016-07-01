9c035146b06abe3f41be74029867f06318184689
xieyi <xieyi_541@163.com>
2016-07-01 12:07:40 +0800

    init

---
+++ src/main/java/xframe/example/datastructure/list/Node.java
@@ -0,0 +1,27 @@
+package xframe.example.datastructure.list;
+
+public class Node<T> {
+
+	private T data;
+	
+	private Node next;
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+	
+	
+	
+}
---
+++ src/main/java/xframe/example/datastructure/list/SeqList.java
@@ -0,0 +1,9 @@
+package xframe.example.datastructure.list;
+
+public class SeqList {
+
+	private static final int MAXSIZE = 20;
+	
+	private int length;
+	
+}
